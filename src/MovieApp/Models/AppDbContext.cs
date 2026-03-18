efcec5aa31b810321ffc6457858314637fd60007
Alexandra-Maria-Chicus <alexandra.chicus@stud.ubbcluj.ro>
2026-03-18 15:59:50 +0200

    KAN-176: added event class and DB init

---
+++ src/MovieApp/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        // This line tells EF Core to create an "Events" table in SQL
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MovieEventsDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ src/MovieApp/Models/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Models
+{
+    public class Event
+    {
+        [Key] // Tells SQL this is the unique ID
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+        public string PosterUrl { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; } = string.Empty;
+
+        [Range(0, double.MaxValue)] 
+        public decimal TicketPrice { get; set; }
+
+        public double HistoricalRating { get; set; }
+        public string EventType { get; set; } = "Movie"; // Default value
+        public int Capacity { get; set; }
+        public string CreatorId { get; set; } = string.Empty;
+    }
+}
