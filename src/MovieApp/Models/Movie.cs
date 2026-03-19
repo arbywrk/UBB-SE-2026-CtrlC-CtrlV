507864f5069dcf6c97bd277ca05434106ce35c3d
Andrei-Borta <andrei.borta@stud.ubbcluj.ro>
2026-03-19 10:58:11 +0200

    add movie related models

---
+++ src/MovieApp/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public int DurationMinutes { get; set; }
+
+        public List<Genre> Genres { get; set; } = new();
+        public List<Actor> Actors { get; set; } = new();
+        public List<Director> Directors { get; set; } = new();
+
+        public double Rating { get; set; }
+    }
+}
