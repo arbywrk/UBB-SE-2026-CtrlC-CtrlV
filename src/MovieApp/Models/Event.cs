e818cd64e71abc6072424659df89e9ba69eead98
Alexandra-Maria-Chicus <alexandra.chicus@stud.ubbcluj.ro>
2026-03-19 11:13:02 +0200

    add event model

--- src/MovieApp/Models/Event.cs
+++ src/MovieApp/Models/Event.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace MovieApp.Models;
 
-namespace MovieApp.Models
+public sealed class Event
 {
-    internal class Event
-    {
-    }
+    public required int Id { get; init; }
+    public required string Title { get; init; }
+    public string? Description { get; init; }
+    public string PosterUrl { get; init; } = "";
+    public required DateTime EventDateTime { get; init; }
+    public required string LocationReference { get; init; }
+    public required decimal TicketPrice { get; init; }
+
+    
+    public double HistoricalRating { get; init; } = 0.0;
+    public required string EventType { get; init; }
+    public int MaxCapacity { get; init; } = 50;
+    public int CurrentEnrollment { get; init; } = 0;
+
+    public required int CreatorUserId { get; init; }
+
+    public int AvailableSpots => MaxCapacity - CurrentEnrollment;
 }
