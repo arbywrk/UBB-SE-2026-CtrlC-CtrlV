efe6b6ac18c8a6e1c125eec11d423020c5fe2a22
Andreea-Maria Buruiana <andreea.maria.buruiana@stud.ubbcluj.ro>
2026-03-23 21:58:02 +0200

    feat(KAN-389): add ITriviaRepository interface

---
+++ src/MovieApp.Core/Repositories/ITriviaRepository.cs
@@ -0,0 +1,9 @@
+﻿using MovieApp.Core.Models;
+
+namespace MovieApp.Core.Repositories
+{
+    public interface ITriviaRepository
+    {
+        Task<IEnumerable<TriviaQuestion>> GetByCategoryAsync(string category, CancellationToken cancellationToken = default);
+    }
+}
